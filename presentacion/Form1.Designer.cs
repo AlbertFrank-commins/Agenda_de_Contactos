a244280b20fa781d482d942f9ca9e8888b68b8be
alejandrocn63 <alejandrocn63@gmail.com>
2025-07-15 22:04:39 -0400

    1.7

--- presentacion/Form1.Designer.cs
+++ presentacion/Form1.Designer.cs
@@ -31,11 +31,18 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.text120 = new System.Windows.Forms.Label();
+            this.btncorreo = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnnombre = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btntelefono = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2DataGridView1 = new Guna.UI2.WinForms.Guna2DataGridView();
             this.guna2Separator1 = new Guna.UI2.WinForms.Guna2Separator();
-            this.btnagregarp = new Guna.UI2.WinForms.Guna2ImageButton();
             this.label1 = new System.Windows.Forms.Label();
             this.btbuscar = new Guna.UI2.WinForms.Guna2TextBox();
             this.guna2Panel1.SuspendLayout();
@@ -44,16 +51,159 @@
             // 
             // guna2Panel1
             // 
+            this.guna2Panel1.Controls.Add(this.text120);
+            this.guna2Panel1.Controls.Add(this.btncorreo);
+            this.guna2Panel1.Controls.Add(this.label2);
+            this.guna2Panel1.Controls.Add(this.label3);
+            this.guna2Panel1.Controls.Add(this.btnnombre);
+            this.guna2Panel1.Controls.Add(this.btntelefono);
+            this.guna2Panel1.Controls.Add(this.guna2Button1);
+            this.guna2Panel1.Controls.Add(this.guna2Button3);
+            this.guna2Panel1.Controls.Add(this.guna2Button2);
             this.guna2Panel1.Controls.Add(this.guna2DataGridView1);
             this.guna2Panel1.Controls.Add(this.guna2Separator1);
-            this.guna2Panel1.Controls.Add(this.btnagregarp);
             this.guna2Panel1.Controls.Add(this.label1);
             this.guna2Panel1.Controls.Add(this.btbuscar);
             this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
             this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.Size = new System.Drawing.Size(577, 1044);
+            this.guna2Panel1.Size = new System.Drawing.Size(1111, 1044);
             this.guna2Panel1.TabIndex = 2;
+            // 
+            // text120
+            // 
+            this.text120.AutoSize = true;
+            this.text120.Location = new System.Drawing.Point(151, 685);
+            this.text120.Name = "text120";
+            this.text120.Size = new System.Drawing.Size(57, 20);
+            this.text120.TabIndex = 50;
+            this.text120.Text = "Correo";
+            // 
+            // btncorreo
+            // 
+            this.btncorreo.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.btncorreo.DefaultText = "";
+            this.btncorreo.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.btncorreo.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.btncorreo.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.btncorreo.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.btncorreo.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btncorreo.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btncorreo.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btncorreo.Location = new System.Drawing.Point(143, 718);
+            this.btncorreo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btncorreo.Name = "btncorreo";
+            this.btncorreo.PlaceholderText = "";
+            this.btncorreo.SelectedText = "";
+            this.btncorreo.Size = new System.Drawing.Size(812, 48);
+            this.btncorreo.TabIndex = 49;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 488);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 48;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(151, 593);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 20);
+            this.label3.TabIndex = 47;
+            this.label3.Text = "Numero de telefono";
+            // 
+            // btnnombre
+            // 
+            this.btnnombre.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.btnnombre.DefaultText = "";
+            this.btnnombre.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.btnnombre.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.btnnombre.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.btnnombre.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.btnnombre.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btnnombre.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnnombre.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btnnombre.Location = new System.Drawing.Point(143, 526);
+            this.btnnombre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnnombre.Name = "btnnombre";
+            this.btnnombre.PlaceholderText = "";
+            this.btnnombre.SelectedText = "";
+            this.btnnombre.Size = new System.Drawing.Size(812, 48);
+            this.btnnombre.TabIndex = 46;
+            // 
+            // btntelefono
+            // 
+            this.btntelefono.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.btntelefono.DefaultText = "";
+            this.btntelefono.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.btntelefono.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.btntelefono.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.btntelefono.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.btntelefono.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btntelefono.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btntelefono.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btntelefono.Location = new System.Drawing.Point(143, 626);
+            this.btntelefono.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btntelefono.Name = "btntelefono";
+            this.btntelefono.PlaceholderText = "";
+            this.btntelefono.SelectedText = "";
+            this.btntelefono.Size = new System.Drawing.Size(812, 43);
+            this.btntelefono.TabIndex = 45;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.AutoRoundedCorners = true;
+            this.guna2Button1.BorderRadius = 28;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(47)))), ((int)(((byte)(47)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Garamond", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(155, 860);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(204, 58);
+            this.guna2Button1.TabIndex = 44;
+            this.guna2Button1.Text = "Guardar";
+            // 
+            // guna2Button3
+            // 
+            this.guna2Button3.AutoRoundedCorners = true;
+            this.guna2Button3.BorderRadius = 28;
+            this.guna2Button3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(47)))), ((int)(((byte)(47)))));
+            this.guna2Button3.Font = new System.Drawing.Font("Garamond", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button3.ForeColor = System.Drawing.Color.White;
+            this.guna2Button3.Location = new System.Drawing.Point(440, 860);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.Size = new System.Drawing.Size(204, 58);
+            this.guna2Button3.TabIndex = 39;
+            this.guna2Button3.Text = "Editar";
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.AutoRoundedCorners = true;
+            this.guna2Button2.BorderRadius = 28;
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(47)))), ((int)(((byte)(47)))));
+            this.guna2Button2.Font = new System.Drawing.Font("Garamond", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.Location = new System.Drawing.Point(716, 860);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(204, 58);
+            this.guna2Button2.TabIndex = 38;
+            this.guna2Button2.Text = "Eliminar";
             // 
             // guna2DataGridView1
             // 
@@ -81,13 +231,13 @@
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.guna2DataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
             this.guna2DataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(189)))), ((int)(((byte)(184)))));
-            this.guna2DataGridView1.Location = new System.Drawing.Point(72, 142);
+            this.guna2DataGridView1.Location = new System.Drawing.Point(134, 137);
             this.guna2DataGridView1.Name = "guna2DataGridView1";
             this.guna2DataGridView1.ReadOnly = true;
             this.guna2DataGridView1.RowHeadersVisible = false;
             this.guna2DataGridView1.RowHeadersWidth = 62;
             this.guna2DataGridView1.RowTemplate.Height = 28;
-            this.guna2DataGridView1.Size = new System.Drawing.Size(425, 785);
+            this.guna2DataGridView1.Size = new System.Drawing.Size(836, 318);
             this.guna2DataGridView1.TabIndex = 9;
             this.guna2DataGridView1.Theme = Guna.UI2.WinForms.Enums.DataGridViewPresetThemes.Red;
             this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(199)))), ((int)(((byte)(195)))));
@@ -115,32 +265,17 @@
             // guna2Separator1
             // 
             this.guna2Separator1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.guna2Separator1.Location = new System.Drawing.Point(72, 98);
+            this.guna2Separator1.Location = new System.Drawing.Point(134, 93);
             this.guna2Separator1.Name = "guna2Separator1";
-            this.guna2Separator1.Size = new System.Drawing.Size(425, 38);
+            this.guna2Separator1.Size = new System.Drawing.Size(836, 38);
             this.guna2Separator1.TabIndex = 8;
-            // 
-            // btnagregarp
-            // 
-            this.btnagregarp.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnagregarp.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
-            this.btnagregarp.HoverState.ImageSize = new System.Drawing.Size(64, 64);
-            this.btnagregarp.Image = ((System.Drawing.Image)(resources.GetObject("btnagregarp.Image")));
-            this.btnagregarp.ImageOffset = new System.Drawing.Point(0, 0);
-            this.btnagregarp.ImageRotate = 0F;
-            this.btnagregarp.IndicateFocus = true;
-            this.btnagregarp.Location = new System.Drawing.Point(72, 927);
-            this.btnagregarp.Name = "btnagregarp";
-            this.btnagregarp.PressedState.ImageSize = new System.Drawing.Size(64, 64);
-            this.btnagregarp.Size = new System.Drawing.Size(135, 114);
-            this.btnagregarp.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Garamond", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(66, 5);
+            this.label1.Location = new System.Drawing.Point(128, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(87, 31);
             this.label1.TabIndex = 6;
@@ -158,7 +293,7 @@
             this.btbuscar.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.btbuscar.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btbuscar.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.btbuscar.Location = new System.Drawing.Point(72, 41);
+            this.btbuscar.Location = new System.Drawing.Point(134, 50);
             this.btbuscar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btbuscar.Name = "btbuscar";
             this.btbuscar.PlaceholderText = "Busca su producto";
@@ -170,7 +305,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 1044);
+            this.ClientSize = new System.Drawing.Size(1111, 1044);
             this.Controls.Add(this.guna2Panel1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -186,9 +321,17 @@
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
         private Guna.UI2.WinForms.Guna2DataGridView guna2DataGridView1;
         private Guna.UI2.WinForms.Guna2Separator guna2Separator1;
-        private Guna.UI2.WinForms.Guna2ImageButton btnagregarp;
         private System.Windows.Forms.Label label1;
         private Guna.UI2.WinForms.Guna2TextBox btbuscar;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private System.Windows.Forms.Label text120;
+        private Guna.UI2.WinForms.Guna2TextBox btncorreo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2TextBox btnnombre;
+        private Guna.UI2.WinForms.Guna2TextBox btntelefono;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
     }
 }
 
--- presentacion/Form1.cs
+++ presentacion/Form1.cs
@@ -16,5 +16,10 @@ namespace presentacion
         {
             InitializeComponent();
         }
+
+        private void btnagregarp_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
